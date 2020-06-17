0dcc39e395cb00c9007121cea614fd2f0a542408
Jumorap <jumorap@unal.edu.co>
2020-06-16 22:47:50 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/sumiser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sumiser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Sumiser'),
+      ),
+      body: new Container(
+        child: new Text('Hello'),
+      ),
+    );
+  }
+}
